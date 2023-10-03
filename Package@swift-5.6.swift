9c4a184c04bf30e35ccf19a9f4d676626e08ff1c
David Young <davidy@passivelogic.com>
2023-10-03 11:07:01 -0400

    Fixing SPM

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -38,7 +38,6 @@ let package = Package(
       .package(url: "https://github.com/apple/swift-log.git", from: "1.4.4"),
       .package(url: "https://github.com/apple/swift-metrics.git", from: "2.1.1"),
       .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
-      .package(name: "TaskSupport", path: "Sources/TaskSupport"),
     ],
     targets: [
       .target(name: "TaskSupport", dependencies: []),
