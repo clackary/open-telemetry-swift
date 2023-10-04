012973d246e703fb08fa6eeda284ca6605dec780
David Young <davidy@passivelogic.com>
2023-10-04 09:46:07 -0400

    Starting Linux work

--- Sources/TaskSupport/LinuxTaskSupport.swift
+++ Sources/TaskSupport/LinuxTaskSupport.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 typealias task_identifier_t = UInt64
+typealias activity_id_t = UInt64
+
+public class LinuxTaskSupport {
+    public func getIdentifiers() -> (task_identifier_t, activity_id_t) {
+        return (0, 0)
+    }
+
+    public func getCurrentIdentifier() -> activity_id_t {
+        return 0
+    }
+
+    public func createActivityContext() -> (activity_id_t, ScopeElement) {
+        let activityState = 0
+
+        return (0, ScopeElement(scope: activityState))
+    }
+
+    public func leaveScope(scope: ScopeElement) {
+        
+    }
+}
 
 #endif
