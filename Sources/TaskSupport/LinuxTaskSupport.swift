ceb45e5783f60974a81620eca6b954efd3b79970
David Young <davidy@passivelogic.com>
2023-10-06 13:02:09 -0400

    Documentation

--- Sources/TaskSupport/LinuxTaskSupport.swift
+++ Sources/TaskSupport/LinuxTaskSupport.swift
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// This class pretends to be the Linux equivalent of Apple's os.activity library. Except that there
-// is no such beast in existence. The best advice we've received thus far was from a very
+// This class pretends to be the Linux equivalent of Apple's os.activity library, but in no way succeeds
+// as there are currently no Linux analogs to Apple's library. The best advice we've received thus far was from a very
 // experienced MacOS developer. He suggested we NOT attempt to re-create that library for Linux. For now,
 // we'll see if libpthread is sufficient. Trouble is, Swift concurrency - async/await (Tasks) - can apparently span
 // OS-level threads on occasion, which means pthread_self() will not always be reliable. Note, however, that other
