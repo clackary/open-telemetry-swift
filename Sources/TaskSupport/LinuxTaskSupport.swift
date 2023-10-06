a92e89ce0e46b807608d631bed2d002e7bcb6fbf
David Young <davidy@passivelogic.com>
2023-10-06 12:54:15 -0400

    Documentation

--- Sources/TaskSupport/LinuxTaskSupport.swift
+++ Sources/TaskSupport/LinuxTaskSupport.swift
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// This class represents what is supposed to be the Linux equivalent of Apple's os.activity library.
-// Except that there is no such beast in existence. The best advice I've received thus far was from a very
-// experienced MacOS developer, who strongly urged we not attempt to re-create that library for Linux.
-// For now, we'll see if libpthread is sufficient. Trouble is, Swift concurrency - async/await (Tasks) -
-// can apparently span OS-level threads on occasion, which means pthread_self() will not always be reliable.
-// Testing will reveal whether or not this will work for our needs.
+// This class pretends to be the Linux equivalent of Apple's os.activity library. Except that there
+// is no such beast in existence. The best advice we've received thus far was from a very
+// experienced MacOS developer. He suggested we NOT attempt to re-create that library for Linux. For now,
+// we'll see if libpthread is sufficient. Trouble is, Swift concurrency - async/await (Tasks) - can apparently span
+// OS-level threads on occasion, which means pthread_self() will not always be reliable. Note, however, that other
+// areas of opentelemetry-swift employ the pthread interface, so dunno. Testing will reveal whether or not this
+// will work for our needs.
 
 #if os(Linux)
 
