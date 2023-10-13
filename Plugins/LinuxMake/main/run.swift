3a9b22710acdda526da1d094cc3e03f1088ec765
David Young <davidy@passivelogic.com>
2023-10-13 15:43:45 -0400

    Plugin should run now

--- Plugins/LinuxMake/main/run.swift
+++ Plugins/LinuxMake/main/run.swift
@@ -1,11 +1,6 @@
 // Copyright (c) 2023 PassiveLogic, Inc.
 
-// This file is the logic implementing our BundleForSims plugin. As of this writing (06/06/2023),
-// the code here functions properly when dependent applications are built within Xcode. We are
-// awaiting further details about how exactly the simulation developers need the generated code
-// bundle to be deployed. At present, the plugin creates both .zip and .tar files containing all
-// packages found in the Packages directory. This may or may not be the required format; after
-// an initial code review, the finishing touches will be applied.
+// This file is the logic implementing our LinuxMake plugin.
 
 import Foundation
 
@@ -21,25 +16,33 @@ enum LinuxMake {
         
         debugPrint("LinuxMake: target is: \(target); cwd is: \(cwd)")
 
-        let rval = make(target)
+        let (cmdout, rval) = make(target)
         
         if (rval == 0) {
-            debugPrint("LinuxMake: make for \(target) successful")
+            debugPrint("LinuxMake: make for \(target) success:")
         } else {
-            debugPrint("LinuxMake: make for \(target) failed")
+            debugPrint("LinuxMake: make for \(target) failed:")
         }
+
+        debugPrint(cmdout)
     }
 
-    static func make(_ target: String) -> Int32 {
+    static func make(_ target: String) -> (String?, Int32) {
         let p = Process()
+        let pipe = Pipe()
 
+        p.standardOutput = pipe
+        p.standardError = pipe
+        
         p.launchPath = "/usr/bin/make"
         p.arguments = target
 
         p.launch()
         p.waitUntilExit()
 
-        return p.terminationStatus
+        let out = String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8)
+
+        return (out, p.terminationStatus)
     }
 
     static func removePath(path: String) throws {
