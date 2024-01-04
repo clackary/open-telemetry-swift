4b69ca9a18e4d7f471b7c5f701dae5b1e5fbfea9
David Young <davidy@passivelogic.com>
2024-01-04 16:47:47 -0500

    Use @discardableResult

--- Sources/OpenTelemetryApi/OpenTelemetryLinux.swift
+++ Sources/OpenTelemetryApi/OpenTelemetryLinux.swift
@@ -8,10 +8,12 @@ public extension OpenTelemetry {
     static var activeSpan: Span? = nil
 
     @_unsafeInheritExecutor
+    @discardableResult
     static func withValueAsync<T>(_ value: Span?, operation: () async throws -> T) async rethrows -> T {
         try await OpenTelemetry.$activeSpan.withValue(value, operation: operation)
     }
 
+    @discardableResult
     static func withValue<T>(_ value: Span?, operation: @escaping () throws -> T) rethrows -> T {
         try OpenTelemetry.$activeSpan.withValue(value, operation: operation)
     }
